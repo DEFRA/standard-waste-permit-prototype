80d1cbe0ced35ad9154d79837676106c1098991b
Martin Glancy <glancyea@gmail.com>
2017-03-10 18:05:22 +0000

    Add first set of routes - needs a check

--- app/routes_v1.js
+++ app/routes_v1.js
@@ -53,17 +53,25 @@ router.post('/preapp/preapp-discussion', function (req, res) {
 
 // Operator ===================================================================
 
-// TODO add conditional
 router.post('/operator/site-operator', function (req, res) {
   res.render(folder + '/operator/site-operator',{
-      "formAction":"/"+ folder + "/operator/company/company-name"
+      "formAction":"/"+ folder + "/operator/checkoperator"
   })
 })
 
-router.post('/operator/company/company-name', function (req, res) {
-  res.render(folder + '/operator/company/company-name',{
-      "formAction":"/"+ folder + "/operator/company/check-company-details"
-  })
+// This is not a real page, just a URL for the route
+router.post('/operator/checkoperator', function (req, res) {
+  if(req.body['operatorType']=="Limited company"){ // think you need square bracket for radios
+    // show company page
+      res.render(folder + '/operator/company/company-name',{
+          "formAction":"/"+ folder + "/operator/company/check-company-details"
+      })
+  } else {
+    // go on to error
+    res.render(folder + '/error/index',{ 
+        "errorText":"We only cover limited companies in this prototype"
+    })
+  }
 })
 
 router.post('/operator/company/check-company-details', function (req, res) {
@@ -111,145 +119,121 @@ router.get('/address/address-manual', function (req, res) {
 
 router.post('/contact/contact-details', function (req, res) {
   res.render(folder + '/contact/contact-details',{
-      "formAction":"/"+ folder + "/upload/site-plan"
+      "formAction":"/"+ folder + "/evidence/upload-site-plan"
   })
 })
 
 
-// Contact ===================================================================
+// Evidence ===================================================================
 
 router.post('/evidence/upload-site-plan', function (req, res) {
-  res.render(folder + '/evidence/upload-site-plan',{    // uses generic upload template
+  res.render(folder + '/evidence/upload-site-plan',{
       "formAction":"/"+ folder + "/evidence/upload-fire-plan"
   })
 })
 
 router.post('/evidence/upload-fire-plan', function (req, res) {
-  res.render(folder + '/upload-fire-plan',{    // uses generic upload template
+  res.render(folder + '/evidence/upload-fire-plan',{ 
       "formAction":"/"+ folder + "/evidence/industry-scheme"
   })
 })
 
 router.post('/evidence/industry-scheme', function (req, res) {
-  res.render(folder + '/evidence/industry-scheme',{    // uses generic upload template
+  res.render(folder + '/evidence/industry-scheme',{
       "formAction":"/"+ folder + "/evidence/upload-technical-evidence"
   })
 })
 
 router.post('/evidence/upload-technical-evidence', function (req, res) {
-  res.render(folder + '/evidence/upload-technical-evidence',{    // uses generic upload template
+  res.render(folder + '/evidence/upload-technical-evidence',{
       "formAction":"/"+ folder + "/evidence/management-system"
   })
 })
 
 router.post('/evidence/management-system', function (req, res) {
-  res.render(folder + '/evidence/management-system',{    // uses generic upload template
+  res.render(folder + '/evidence/management-system',{
       "formAction":"/"+ folder + "/evidence/declare-offences"
   })
 })
 
 router.post('/evidence/declare-offences', function (req, res) {
-  res.render(folder + '/evidence/declare-offences',{    // uses generic upload template
-      "formAction":"/"+ folder + "/evidence/bankruptcy-insolvency"
+  res.render(folder + '/evidence/declare-offences',{
+      "formAction":"/"+ folder + "/evidence/offencescheck"
   })
 })
 
-
-
-router.post('/evidence/declare-offences', function (req, res) {
-  if(req.body.offences=="yes"){
+// This is not a real page, just a URL for the route
+router.post('/evidence/offencescheck', function (req, res) {
+  if(req.body['offences']=="yes"){ // think you need square bracket for radios
     // show details page
-      res.render(folder + '/evidence/details-of-offence',{    // uses generic upload template
+      res.render(folder + '/evidence/details-of-offence',{
           "formAction":"/"+ folder + "/evidence/bankruptcy-insolvency"
       })
-  } else{
+  } else {
     // go on to bankruptcy
-    res.render(folder + '/evidence/details-of-offence',{    // uses generic upload template
-        "formAction":"/"+ folder + "/evidence/bankruptcy-insolvency"
+    res.render(folder + '/evidence/bankruptcy-insolvency',{ 
+        "formAction":"/"+ folder + "/check/claim-confidentiality"
     })
   }
 })
 
-// <li><a href="/v1/evidence/declare-offences">Do you have any offences to declare?</a></li>
-// <li><a href="/v1/evidence/details-of-offence">Details of the offence</a></li>
-// <li><a href="/v1/evidence/bankruptcy-insolvency">Tell us about any bankruptcy or insolvency</a></li>
-
-// <li><a href="/v1/check/claim-confidentiality">Do you want to claim confidentiality</a></li>
-// <li><a href="/v1/check/check-answers">Check your answers</a></li>
-
-// <li><a href="/v1/pay/payment-method">Choose how you want to pay</a></li>
-// <li><a href="/v1/pay/pay-by-cheque">Pay by cheque</a></li>
-// <li><a href="/v1/pay/enter-card-details">Pay - enter card details</a></li>
-// <li><a href="/v1/pay/confirm-payment">Pay - confirm payment</a></li>
-
-// <li><a href="/v1/done/index">Confirmation (done) page</a></li>
-// <li><a href="/v1/done/email-confirm">Confirmation email</a></li>
-
-
-// add your routes here
-
-// About you ***************************************
-
-// router.get URL must match the HTML form action URL
-/*
-router.get('/about-you/applicant-type/application-from-a-company', function (req, res) {
-  // get the answer from the query string
-  var applicantType = req.query.applicantType
-
-  if (applicantType === 'An individual') {
-    res.redirect('/' + folder +'/about-you/applicant-type/application-from-individual')
-  } 
-
-  else if (applicantType === 'An organisation of individuals') {
-    res.redirect('/' + folder +'/about-you/applicant-type/application-from-org-of-individuals')
-  } 
-
-  else if (applicantType === 'A public body') {
-    res.redirect('/' + folder +'/about-you/applicant-type/application-from-public-body')
-  } 
-
-  else {
-    // if applicantType is any other value (or is missing) render the page requested. 
-    // Important - don't include the slash at the start
-    res.render('/' + folder +'about-you/applicant-type/application-from-a-company')
-  }
+router.post('/evidence/bankruptcy-insolvency', function (req, res) {
+  res.render(folder + '/evidence/bankruptcy-insolvency',{
+      "formAction":"/"+ folder + "/check/claim-confidentiality"
+  })
 })
-*/
 
+router.post('/check/claim-confidentiality', function (req, res) {
+  res.render(folder + '/check/claim-confidentiality',{
+      "formAction":"/"+ folder + "/check/check-answers"
+  })
+})
 
-  // Is your main UK business address different from above?
+router.post('/check/check-answers', function (req, res) {
+  res.render(folder + '/check/check-answers',{
+      "formAction":"/"+ folder + "/pay/payment-method"
+  })
+})
 
-router.get('/about-you/contact-application', function (req, res) {
-  // get the answer from the query string (eg. ?UKBusinessAddress=no)
-  var UKBusinessAddress = req.query.UKBusinessAddress
+router.post('/pay/payment-method', function (req, res) {
+  res.render(folder + '/pay/payment-method',{
+      "formAction":"/"+ folder + "/pay/how-to-pay"
+  })
+})
 
-  if (UKBusinessAddress === 'Yes') {
-    // redirect to the relevant page
-    res.redirect('/' + folder +'/about-you/main-UK-business-address')
+// This is not a real page, just a URL for the route
+router.post('/pay/how-to-pay', function (req, res) {
+  if(req.body['paymentMethod']=="Debit or credit card"){ // think you need square bracket for radios
+    // show details page
+      res.render(folder + '/pay/enter-card-details',{
+          "formAction":"/"+ folder + "/pay/confirm-payment"
+      })
+  } else if(req.body['paymentMethod']=="Bank transfer") {
+    // go on to bankruptcy
+    res.render(folder + '/pay/pay-by-bank-transfer',{ 
+        "formAction":"/"+ folder + "/done/index"
+    })
   } else {
-    // if UKBusinessAddress is any other value (or is missing) render the page requested.
-    // Remember to omit the forward-slash at the start
-    res.render('/' + folder +'about-you/contact-application')
+    // go on to cheque
+    res.render(folder + '/pay/pay-by-cheque',{ 
+        "formAction":"/"+ folder + "/done/index"
+    })
   }
 })
 
-
-  // Who can we contact about your operation?
-
-router.get('/about-you/contact-billing', function (req, res) {
-  // get the answer from the query string (eg. ?OperationContact=no)
-  var OperationContact = req.query.OperationContact
-
-  if (OperationContact === 'different') {
-    // redirect to the relevant page
-    res.redirect('/' + folder +'/about-you/contact-operation')
-  } else {
-    // if OperationContact is any other value (or is missing) render the page requested.
-    // Remember to omit the forward-slash at the start
-    res.render('/' + folder +'about-you/contact-billing')
-  }
+router.post('/pay/confirm-payment', function (req, res) {
+  res.render(folder + '/pay/confirm-payment',{
+      "formAction":"/"+ folder + "/done/index"
+  })
 })
 
+router.post('/done/index', function (req, res) {
+  res.render(folder + '/done/index',{
+      "formAction":"NOT_NEEDED"
+  })
+})
+
+// No route needed for /v1/done/email-confirm">Confirmation email
 
 
 
