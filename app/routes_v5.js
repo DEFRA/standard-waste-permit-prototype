0a4f5fb35fc97e7268912e9d8bb7fd0d4fa5ce9b
Martin Glancy <glancyea@gmail.com>
2017-05-19 15:36:38 +0100

    Add route so user can select from GOV.UK page

--- app/routes_v5.js
+++ app/routes_v5.js
@@ -40,7 +40,7 @@ router.get('/start/rules-page', function (req, res) {
 router.get('/check/process-link', function (req, res) {
   for(var input in req.body) req.session.permit[input] = req.body[input] // form to session
     res.render(folder + '/check/process-link',{ // show save and return pages
-       "formAction":"/"+ folder + "/check/task-list",
+       "formAction":"/"+ folder + "/start/start-or-resume",
        "chosenPermitID":req.query['chosenPermitID'],
        "permit":req.session.permit // always send permit object to page
     })
@@ -112,11 +112,21 @@ router.get('save-and-return/email-save-link', function (req, res) {
 
 router.post('/selectpermit/choose-expanding-sections', function (req, res) {
   for(var input in req.body) req.session.permit[input] = req.body[input] 
-    res.render(folder + '/selectpermit/choose-expanding-sections',{
-      "formAction":"/"+ folder + "/check/save-permit-details",
-      "chosenCategory":req.body['chosenCategory'],
-      "permit":req.session.permit // always send permit object to page
-    }) 
+    // permit NOT YET selected
+    if( req.session.permit['chosenPermitID']==null ) {
+      res.render(folder + '/selectpermit/choose-expanding-sections',{
+        "formAction":"/"+ folder + "/check/save-permit-details",
+        "chosenCategory":req.body['chosenCategory'],
+        "permit":req.session.permit // always send permit object to page
+      })
+    // permit set via link on a GOV.UK page so skip this page
+    } else {
+      res.render(folder + '/check/task-list',{ // show save and return pages
+         "formAction":"/"+ folder + "/check/check-answers",
+         "chosenPermitID":req.body['chosenPermitID'],
+         "permit":req.session.permit // always send permit object to page
+      })
+    }
 })
 
 // required for 'select a different permit' via task list
