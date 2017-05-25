0903a241c9c376f51d6fde4ce7804192d68f7b54
Martin Glancy <glancyea@gmail.com>
2017-05-25 10:49:30 +0100

    Add v6 folder, routes and index

--- app/routes.js
+++ app/routes.js
@@ -8,6 +8,7 @@ router.use('/v2a', require('./routes_v2a'));
 router.use('/v3', require('./routes_v3'));
 router.use('/v4', require('./routes_v4'));
 router.use('/v5', require('./routes_v5'));
+router.use('/v6', require('./routes_v6'));
 
 // Route index page
 router.get('/', function (req, res) {
