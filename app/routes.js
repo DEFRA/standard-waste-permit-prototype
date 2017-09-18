4bb9a13c3efe0edb0e4395e943128d9bc0c9b73b
Martin Glancy <glancyea@gmail.com>
2017-09-18 10:46:53 +0100

    Add v8 folder and update required files and links

--- app/routes.js
+++ app/routes.js
@@ -10,6 +10,7 @@ router.use('/v4', require('./routes_v4'));
 router.use('/v5', require('./routes_v5'));
 router.use('/v6', require('./routes_v6'));
 router.use('/v7', require('./routes_v7'));
+router.use('/v8', require('./routes_v8'));
 
 
 // Route index page
