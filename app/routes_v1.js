7ac656e24fea2d3243ffda8cff7cc73842b0f4b7
Christina Gyles <c.gyles@live.co.uk>
2018-03-12 08:07:41 +0000

    comment in routes v1 commit

--- app/routes_v1.js
+++ app/routes_v1.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var router = express.Router()
 
+// this is a comment as a test
 // this file deals with all paths starting /version_x
 // How to use folder variable:
 // res.redirect( '/' + folder + '/exemptions/add_exemptions');
@@ -10,14 +11,14 @@ router.use(function (req, res, next) {
   // set a folder and store oin locals
   // this can then be used in pages as {{folder}}
   res.locals.folder=folder
-  
+
   // check for an existing permit object stored in session
   // if it does not exist, create it
   var permit = req.session.permit
   if (!permit) {
     permit = req.session.permit = {}
   }
-  
+
   next()
 });
 
@@ -34,25 +35,25 @@ router.get('/selectpermit/permit-category', function (req, res) {
 
 router.post('/selectpermit/choose-permit', function (req, res) {
   if(typeof req.body['chosenCategory']==='undefined'){  // simple error handling
-    res.render(folder + '/error/index',{ 
+    res.render(folder + '/error/index',{
         "errorText":"Please say what you want the permit for"
     })
   } else {
     res.render(folder + '/selectpermit/choose-permit',{
       "formAction":"/"+ folder + "/selectpermit/check-permit",
       "chosenCategory":req.body['chosenCategory']
-    })    
+    })
   }
 })
 
 router.post('/selectpermit/check-permit', function (req, res) {
   if(typeof req.body['chosenPermitID']==='undefined'){  // simple error handling
-    res.render(folder + '/error/index',{ 
+    res.render(folder + '/error/index',{
         "errorText":"Please select a permit"
     })
   } else {
     // save chosen Permit ID in session
-    for(var input in req.body) req.session.permit[input] = req.body[input] // add form entries to session 
+    for(var input in req.body) req.session.permit[input] = req.body[input] // add form entries to session
     res.render(folder + '/selectpermit/check-permit',{
       "formAction":"/"+ folder + "/selectpermit/what-need-to-apply",
       "chosenPermitID":req.body['chosenPermitID'],
@@ -62,7 +63,7 @@ router.post('/selectpermit/check-permit', function (req, res) {
 })
 
 router.post('/selectpermit/what-need-to-apply', function (req, res) {
-  for(var input in req.body) req.session.permit[input] = req.body[input] // add form entries to session 
+  for(var input in req.body) req.session.permit[input] = req.body[input] // add form entries to session
   res.render(folder + '/selectpermit/what-need-to-apply',{
       "formAction":"/"+ folder + "/preapp/preapp-discussion",
       "chosenPermitID":req.body['chosenPermitID'],
@@ -74,7 +75,7 @@ router.post('/selectpermit/what-need-to-apply', function (req, res) {
 // Pre-app ===================================================================
 
 router.post('/preapp/preapp-discussion', function (req, res) {
-  for(var input in req.body) req.session.permit[input] = req.body[input] // add form entries to session 
+  for(var input in req.body) req.session.permit[input] = req.body[input] // add form entries to session
   res.render(folder + '/preapp/preapp-discussion',{
       "formAction":"/"+ folder + "/operator/site-operator",
       "permit":req.session.permit // always send permit object to page
@@ -85,7 +86,7 @@ router.post('/preapp/preapp-discussion', function (req, res) {
 // Operator ===================================================================
 
 router.post('/operator/site-operator', function (req, res) {
-  for(var input in req.body) req.session.permit[input] = req.body[input] // add form entries to session 
+  for(var input in req.body) req.session.permit[input] = req.body[input] // add form entries to session
   res.render(folder + '/operator/site-operator',{
       "formAction":"/"+ folder + "/operator/checkoperator",
       "permit":req.session.permit // always send permit object to page
@@ -94,7 +95,7 @@ router.post('/operator/site-operator', function (req, res) {
 
 // This is not a real page, just a URL for the route
 router.post('/operator/checkoperator', function (req, res) {
-  for(var input in req.body) req.session.permit[input] = req.body[input] // add form entries to session 
+  for(var input in req.body) req.session.permit[input] = req.body[input] // add form entries to session
   if(req.body['operatorType']=="Limited company"){ // think you need square bracket for radios
     // show company page
       res.render(folder + '/operator/company/company-name',{
@@ -103,14 +104,14 @@ router.post('/operator/checkoperator', function (req, res) {
       })
   } else {
     // go on to error
-    res.render(folder + '/error/index',{ 
+    res.render(folder + '/error/index',{
         "errorText":"We only cover limited companies in this prototype"
     })
   }
 })
 
 router.post('/operator/company/check-company-details', function (req, res) {
-  for(var input in req.body) req.session.permit[input] = req.body[input] // add form entries to session 
+  for(var input in req.body) req.session.permit[input] = req.body[input] // add form entries to session
   res.render(folder + '/operator/company/check-company-details',{
       "formAction":"/"+ folder + "/site/site-name",
       "permit":req.session.permit // always send permit object to page
@@ -121,7 +122,7 @@ router.post('/operator/company/check-company-details', function (req, res) {
 // Site ===================================================================
 
 router.post('/site/site-name', function (req, res) {
-  for(var input in req.body) req.session.permit[input] = req.body[input] // add form entries to session 
+  for(var input in req.body) req.session.permit[input] = req.body[input] // add form entries to session
   res.render(folder + '/site/site-name',{
       "formAction":"/"+ folder + "/site/grid-reference",
       "permit":req.session.permit // always send permit object to page
@@ -129,7 +130,7 @@ router.post('/site/site-name', function (req, res) {
 })
 
 router.post('/site/grid-reference', function (req, res) {
-  for(var input in req.body) req.session.permit[input] = req.body[input] // add form entries to session 
+  for(var input in req.body) req.session.permit[input] = req.body[input] // add form entries to session
   res.render(folder + '/site/grid-reference',{
       "formAction":"/"+ folder + "/address/postcode",
       "permit":req.session.permit // always send permit object to page
@@ -137,7 +138,7 @@ router.post('/site/grid-reference', function (req, res) {
 })
 
 router.post('/address/postcode', function (req, res) {
-  for(var input in req.body) req.session.permit[input] = req.body[input] // add form entries to session 
+  for(var input in req.body) req.session.permit[input] = req.body[input] // add form entries to session
   res.render(folder + '/address/postcode',{
       "formAction":"/"+ folder + "/address/address",
       "permit":req.session.permit // always send permit object to page
@@ -145,7 +146,7 @@ router.post('/address/postcode', function (req, res) {
 })
 
 router.post('/address/address', function (req, res) {
-  for(var input in req.body) req.session.permit[input] = req.body[input] // add form entries to session 
+  for(var input in req.body) req.session.permit[input] = req.body[input] // add form entries to session
   res.render(folder + '/address/address',{
       "formAction":"/"+ folder + "/contact/contact-details",
       "permit":req.session.permit // always send permit object to page
@@ -164,7 +165,7 @@ router.get('/address/address-manual', function (req, res) {
 // Contact ===================================================================
 
 router.post('/contact/contact-details', function (req, res) {
-  for(var input in req.body) req.session.permit[input] = req.body[input] // add form entries to session 
+  for(var input in req.body) req.session.permit[input] = req.body[input] // add form entries to session
   res.render(folder + '/contact/contact-details',{
       "formAction":"/"+ folder + "/evidence/upload-site-plan",
       "permit":req.session.permit // always send permit object to page
@@ -175,7 +176,7 @@ router.post('/contact/contact-details', function (req, res) {
 // Evidence ===================================================================
 
 router.post('/evidence/upload-site-plan', function (req, res) {
-  for(var input in req.body) req.session.permit[input] = req.body[input] // add form entries to session 
+  for(var input in req.body) req.session.permit[input] = req.body[input] // add form entries to session
   res.render(folder + '/evidence/upload-site-plan',{
       "formAction":"/"+ folder + "/evidence/upload-fire-plan",
       "permit":req.session.permit // always send permit object to page
@@ -183,15 +184,15 @@ router.post('/evidence/upload-site-plan', function (req, res) {
 })
 
 router.post('/evidence/upload-fire-plan', function (req, res) {
-  for(var input in req.body) req.session.permit[input] = req.body[input] // add form entries to session 
-  res.render(folder + '/evidence/upload-fire-plan',{ 
+  for(var input in req.body) req.session.permit[input] = req.body[input] // add form entries to session
+  res.render(folder + '/evidence/upload-fire-plan',{
       "formAction":"/"+ folder + "/evidence/industry-scheme",
       "permit":req.session.permit // always send permit object to page
   })
 })
 
 router.post('/evidence/industry-scheme', function (req, res) {
-  for(var input in req.body) req.session.permit[input] = req.body[input] // add form entries to session 
+  for(var input in req.body) req.session.permit[input] = req.body[input] // add form entries to session
   res.render(folder + '/evidence/industry-scheme',{
       "formAction":"/"+ folder + "/evidence/upload-technical-evidence",
       "permit":req.session.permit // always send permit object to page
@@ -199,7 +200,7 @@ router.post('/evidence/industry-scheme', function (req, res) {
 })
 
 router.post('/evidence/upload-technical-evidence', function (req, res) {
-  for(var input in req.body) req.session.permit[input] = req.body[input] // add form entries to session 
+  for(var input in req.body) req.session.permit[input] = req.body[input] // add form entries to session
   res.render(folder + '/evidence/upload-technical-evidence',{
       "formAction":"/"+ folder + "/evidence/management-system",
       "permit":req.session.permit // always send permit object to page
@@ -207,7 +208,7 @@ router.post('/evidence/upload-technical-evidence', function (req, res) {
 })
 
 router.post('/evidence/management-system', function (req, res) {
-  for(var input in req.body) req.session.permit[input] = req.body[input] // add form entries to session 
+  for(var input in req.body) req.session.permit[input] = req.body[input] // add form entries to session
   res.render(folder + '/evidence/management-system',{
       "formAction":"/"+ folder + "/evidence/declare-offences",
       "permit":req.session.permit // always send permit object to page
@@ -215,7 +216,7 @@ router.post('/evidence/management-system', function (req, res) {
 })
 
 router.post('/evidence/declare-offences', function (req, res) {
-  for(var input in req.body) req.session.permit[input] = req.body[input] // add form entries to session 
+  for(var input in req.body) req.session.permit[input] = req.body[input] // add form entries to session
   res.render(folder + '/evidence/declare-offences',{
       "formAction":"/"+ folder + "/evidence/offencescheck",
       "permit":req.session.permit // always send permit object to page
@@ -224,7 +225,7 @@ router.post('/evidence/declare-offences', function (req, res) {
 
 // This is not a real page, just a URL for the route
 router.post('/evidence/offencescheck', function (req, res) {
-  for(var input in req.body) req.session.permit[input] = req.body[input] // add form entries to session 
+  for(var input in req.body) req.session.permit[input] = req.body[input] // add form entries to session
   if(req.body['offences']=="yes"){ // think you need square bracket for radios
     // show details page
       res.render(folder + '/evidence/details-of-offence',{
@@ -233,7 +234,7 @@ router.post('/evidence/offencescheck', function (req, res) {
       })
   } else {
     // go on to bankruptcy
-    res.render(folder + '/evidence/bankruptcy-insolvency',{ 
+    res.render(folder + '/evidence/bankruptcy-insolvency',{
         "formAction":"/"+ folder + "/check/claim-confidentiality",
         "permit":req.session.permit // always send permit object to page
     })
@@ -241,7 +242,7 @@ router.post('/evidence/offencescheck', function (req, res) {
 })
 
 router.post('/evidence/bankruptcy-insolvency', function (req, res) {
-  for(var input in req.body) req.session.permit[input] = req.body[input] // add form entries to session 
+  for(var input in req.body) req.session.permit[input] = req.body[input] // add form entries to session
   res.render(folder + '/evidence/bankruptcy-insolvency',{
       "formAction":"/"+ folder + "/check/claim-confidentiality",
       "permit":req.session.permit // always send permit object to page
@@ -249,7 +250,7 @@ router.post('/evidence/bankruptcy-insolvency', function (req, res) {
 })
 
 router.post('/check/claim-confidentiality', function (req, res) {
-  for(var input in req.body) req.session.permit[input] = req.body[input] // add form entries to session 
+  for(var input in req.body) req.session.permit[input] = req.body[input] // add form entries to session
   res.render(folder + '/check/claim-confidentiality',{
       "formAction":"/"+ folder + "/check/check-answers",
       "permit":req.session.permit // always send permit object to page
@@ -257,7 +258,7 @@ router.post('/check/claim-confidentiality', function (req, res) {
 })
 
 router.post('/check/check-answers', function (req, res) {
-  for(var input in req.body) req.session.permit[input] = req.body[input] // add form entries to session 
+  for(var input in req.body) req.session.permit[input] = req.body[input] // add form entries to session
   res.render(folder + '/check/check-answers',{
       "formAction":"/"+ folder + "/pay/payment-method",
       "permit":req.session.permit // always send permit object to page
@@ -281,13 +282,13 @@ router.post('/pay/how-to-pay', function (req, res) {
       })
   } else if(req.body['paymentMethod']=="Bank transfer") {
     // go on to bankruptcy
-    res.render(folder + '/pay/pay-by-bank-transfer',{ 
+    res.render(folder + '/pay/pay-by-bank-transfer',{
         "formAction":"/"+ folder + "/done/index",
         "permit":req.session.permit // always send permit object to page
     })
   } else {
     // go on to cheque
-    res.render(folder + '/pay/pay-by-cheque',{ 
+    res.render(folder + '/pay/pay-by-cheque',{
         "formAction":"/"+ folder + "/done/index",
         "permit":req.session.permit // always send permit object to page
     })
@@ -314,7 +315,7 @@ router.post('/done/index', function (req, res) {
 // Errors ===================================================================
 
 router.get('/error/wrong-company-details', function (req, res) {
-    res.render(folder + '/error/index',{ 
+    res.render(folder + '/error/index',{
         "errorText":"If the company details are not correct you can't apply online. Please contact us."
     })
 })
