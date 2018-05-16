85fe8d0fe968a05c96190257f290196b5275bcb1
Christina Gyles <christinagyles@Christinas-MacBook-Pro.local>
2018-05-16 16:36:18 +0100

    content updates and if else on members dates of birth or company name

--- app/filters.js
+++ app/filters.js
@@ -37,15 +37,19 @@ module.exports = function (env) {
     documentation.
 
   ------------------------------------------------------------------ */
-  
+
   filters.nl2br = function(textStr) {
       return textStr.replace(/\r|\n|\r\n/g, '<br />')
   }
-  
+
   filters.reverseName = function(textStr) {
-    var parts = textStr.split(',')
-    return parts[1] + ' ' + parts[0]
-  }
+     var parts = textStr.split(',')
+     if(parts[1]===undefined){
+       return parts[0]
+     } else {
+       return parts[1] + ' ' + parts[0]
+     }
+   }
 
   /* ------------------------------------------------------------------
     keep the following line to return your filters to the app
