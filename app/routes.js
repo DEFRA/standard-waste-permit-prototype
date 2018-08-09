d91a7114f2a8be30daa4f1b06fc3bb4ce0a926d3
Martin Glancy <glancyea@gmail.com>
2018-08-09 12:20:36 +0100

    Minor updates to activity selection

--- app/routes.js
+++ app/routes.js
@@ -6,6 +6,7 @@ router.use(function (req, res, next) {
   next()
 });
 
+
 // anything beginning with "/vn" will go into this
 router.use('/v1', require('./routes_v1'))
 router.use('/v2', require('./routes_v2'))
