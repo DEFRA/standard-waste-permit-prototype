59787a1013df9debc61089ec8fad8c0f32e65618
Martin Glancy <glancyea@gmail.com>
2018-09-28 09:21:08 +0100

    Dust plan

--- app/routes_v19.js
+++ app/routes_v19.js
@@ -1900,6 +1900,32 @@ router.all('/upload-odour-management-plan', function (req, res) {
   }
 })
 
+// DUST MANAGEMENT PLAN UPLOAD ========================================================
+router.all('/upload-dust-management-plan', function (req, res) {
+  var path="/upload-dust-management-plan"
+  var title="Upload the dust management plan"
+  var fileName="DustPlan"
+  var guidanceTop="dustplantop"
+  var guidanceBot=""
+  var fileTypes="PDF, DOC, DOCX or JPG"
+  
+  if ( typeof req.session.data['visitCount'] === 'undefined' ) {
+    var visitCount = 1
+  } else {
+    var visitCount = parseInt(req.session.data['visitCount'])+1
+  }
+  
+  if( visitCount===1 || visitCount===2 || req.session.data['uploadOtherFile']=="yes" ){
+    res.render(folder + '/upload/upload-file',{"title":title,"fileName":fileName,"visitCount":visitCount,"guidanceTop":guidanceTop,"guidanceBot":guidanceBot,"formAction":"/"+ folder + path,"fileTypes":fileTypes})
+  } else {
+    delete req.session.data['visitCount']
+    // Where to go after the files have been uploaded
+    res.render(folder + "/check/task-list",{
+          "formAction":"/"+ folder + "/check/check-answers"
+    })
+  }
+})
+
 
 
 // Send permit data in session to every page ==================================
